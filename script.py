05c6b75a85de3593a57aeafdfdbeff98aacf322d
elnathanau1 <elnathanau1@gmail.com>
2019-06-09 22:36:11 -0700

    added req, move download to func

--- script.py
+++ script.py
@@ -1,25 +1,23 @@
 import os.path
 import requests
+from clint.textui import progress
 
 # Android termux path:
 save_path = "/storage/524C-AE60/Android/data/com.termux/Comics/"
-# save_path = "/Users/eau/Documents/"
+# SAVE_PATH = "/Users/eau/Documents/"
+
+def download_comic(link, name):
+    complete_name = os.path.join(SAVE_PATH, name)
+    r = requests.get(download_link, allow_redirects=True, stream=True)
+
+    # code from https://stackoverflow.com/questions/15644964/python-progress-bar-and-downloads
+    with open(complete_name, 'wb') as f:
+        total_length = int(r.headers.get('content-length'))
+        for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
+            if chunk:
+                f.write(chunk)
+                f.flush()
 
 # Ascender download link
-download_link = "https://getcomics.info/go.php-urls/aHR0cDovL3Rlbi5jb21pY2ZpbGVzLnJ1L1dlZWtseSUyMFJlbGVhc2UvMjAxOS4wNS4yOS9VcDEvQXNjZW5kZXIlMjAwMDIlMjAlMjgyMDE5JTI5JTIwJTI4RGlnaXRhbCUyOSUyMCUyOFpvbmUtRW1waXJlJTI5LmNicg=="
-complete_name = os.path.join(save_path, "ascender.cbr")
-
-r = requests.get(download_link, allow_redirects=True)
-open(complete_name, 'wb').write(r.content)
-
-# name_of_file = input("What is the name of the file: ")
-#
-# completeName = os.path.join(save_path, name_of_file+".txt")
-#
-# file1 = open(completeName, "w")
-#
-# toFile = input("Write what you want into the field: ")
-#
-# file1.write(toFile)
-#
-# file1.close()
+download_link = "https://getcomics.info/go.php-urls/aHR0cDovL3Rlbi5jb21pY2ZpbGVzLnJ1L090aGVycy9JbWFnZSUyMENvbWljcy9DaGV3L0NoZXclMjB2MDElMjAtJTIwVGFzdGVyJTI3cyUyMENob2ljZSUyMCUyODIwMDklMjklMjBHZXRDb21pY3MuSU5GTy5jYnI="
+download_comic(download_link, "Chew Vol 1.cbr")
